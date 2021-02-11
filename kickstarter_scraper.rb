fd693c767a19a147820c40bba129b654a2068d06
danny galimi <dannygalimi13@aim.com>
2021-02-11 23:15:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,17 +2,16 @@ require "nokogiri"
 require "pry"
  
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
- 
   projects = {}
-  
- kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end
-  
-  projects 
-end 
+ 
+  # return the projects hash
+  projects
+end
   
 
 
